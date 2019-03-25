bfc290dfe3bfb1835add01a9b9f49f0d83d6caf2
ka1myk <kalmyk.mp3@gmail.com>
2019-03-25 11:00:08 +0300

    03/25/19 11:00:08

---
+++ mtr.maunty.ru/template/mt/html5-ie.js
@@ -0,0 +1,2 @@
+// JavaScript Document
+ var e = ("article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(','); for(var i = 0; i < e.length; i++) {document.createElement(e[i]);}
